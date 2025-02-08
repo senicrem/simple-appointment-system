54f0982011e2c0f72cb3d86690e1eee893ed4e62
JohnJay <mercinesjohnjay@gmail.com>
2025-02-08 14:18:16 +0800

    admin files

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html class=h-full>
+  <head>
+    <title>Appointment System | Admin</title>
+    <link rel="icon" type="image/x-icon" href="/favicon_io/favicon.ico">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+       @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+      <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    @endif
+  </head>
+  <body class="bg-linear-to-t from-sky-500 to-indigo-500">
+      <div class="flex mt-32 justify-center">
+        <livewire:admin-page />
+      </div>
+  </body>
+</html>
