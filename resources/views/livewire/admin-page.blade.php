de110f60761b8953a01164f91fffd1af9cadf624
JohnJay <mercinesjohnjay@gmail.com>
2025-02-14 13:24:00 +0800

    add column in appoitment table

--- resources/views/livewire/admin-page.blade.php
+++ resources/views/livewire/admin-page.blade.php
@@ -6,14 +6,14 @@
             @foreach($appointments as $appointment)
                 @foreach($appointment as $p)
                 <div class="group">
-                    <div class="text-sm p-2 border-b-1 border-slate-800 group-hover:bg-slate-200 transition-all delay-200">
+                    <div class="text-sm p-2 border-b-1 border-slate-800 group-hover:bg-slate-200 transition-all delay-100">
                         <div class="flex justify-between">
                             <p class="text-lg">{{ $p['name'] }}</p>
                             <div class="text-lg">{{ $p['scheduled_date'] }}</div>
                         </div>
                         <div class="flex justify-between">
                             <p>{{$p['concern']}}</p>
-                            <div class="flex gap-2 opacity-0 group-hover:opacity-100 transition-opacity delay-200">
+                            <div class="flex gap-2 opacity-0 group-hover:opacity-100 transition-opacity delay-100">
                                 <button class="bg-green-700 px-3 py-1 text-white cursor-pointer hover:bg-green-600">Done</button>
                                 <button class="bg-red-700 px-3 py-1 text-white cursor-pointer hover:bg-red-600">Cancel</button>
                             </div>
