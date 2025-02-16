a030ae8fa360f322c183d295184f7336d5c5f51b
JohnJay <mercinesjohnjay@gmail.com>
2025-02-16 11:55:35 +0800

    add change month func

--- app/Livewire/AdminPage.php
+++ app/Livewire/AdminPage.php
@@ -4,14 +4,34 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Appointment;
+use Carbon\Carbon;
 
 class AdminPage extends Component
 {
     public array $appointments = [];
+    public int $month = 0;
 
     public function mount () {
-        $this->appointments =  Appointment::forMonth(2)->groupBy('scheduled_date')->toArray();
+        $currMonth = Carbon::now()->format('m');
+
+        $this->month = $currMonth;
+        $this->setSessionsByMonth($currMonth);
     }
+
+    public function setSessionsByMonth (int $month) {
+        $sessions = Appointment::forMonth($month)
+            ->groupBy('scheduled_date')
+            ->toArray();
+
+        $this->appointments = $sessions;
+    }
+
+    public function changeMonth ($optVal) : void {
+        $month = (int) $optVal;
+
+        $this->month = $month;
+        $this->setSessionsByMonth($month);
+    }   
 
     
     public function render()
